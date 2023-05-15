3bc9437f8485812f808fe00096e44b2dd7a37656
Zehad-Sarkar <zehadsarkar22@gmail.com>
2023-05-15 13:00:00 +0600

    pagination completed

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -13,39 +13,63 @@ const Shop = () => {
   const [cart, setCart] = useState([]);
   const { totalProducts } = useLoaderData(); //for pagination
   const [currentPage, setCurrentPage] = useState(0);
-  const [itemsperPages, setItemsPerPages] = useState(10);
+  const [itemsPerPages, setItemsPerPages] = useState(10);
 
   //for pagination *start*
   // const itemsperPages = 10;
-  const totalPages = Math.ceil(totalProducts / itemsperPages);
+  const totalPages = Math.ceil(totalProducts / itemsPerPages);
   const pagesNumber = [...Array(totalPages).keys()];
-  const options = [5, 10, 20];
+  const options = [5, 10, 15, 20];
 
-  const handleSelectChange = e => {
-    setItemsPerPages(parseInt(e.target.value))
-    setCurrentPage(0)
-  }
-//pagination end here
-  
+  const handleSelectChange = (e) => {
+    setItemsPerPages(parseInt(e.target.value));
+    setCurrentPage(0);
+  };
   useEffect(() => {
-    fetch("http://localhost:5000/products")
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
-  }, []);
+    async function fetchData() {
+      const response = await fetch(
+        `http://localhost:5000/products?page=${currentPage}&limit=${itemsPerPages}`
+      );
+      const data = await response.json();
+      setProducts(data);
+    }
+    fetchData();
+  }, [currentPage, itemsPerPages]);
+  //pagination end here
+
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/products")
+  //     .then((res) => res.json())
+  //     .then((data) => setProducts(data));
+  // }, []);
 
   useEffect(() => {
     let storedCart = getShoppingCart();
-    let savedCart = [];
-    for (const id in storedCart) {
-      const addedProduct = products.find((product) => product._id === id);
-      if (addedProduct) {
-        const quantity = storedCart[id];
-        addedProduct.quantity = quantity;
-        savedCart.push(addedProduct);
-      }
-    }
-    setCart(savedCart);
-  }, [products]);
+    const ids = Object.keys(storedCart);
+    fetch("http://localhost:5000/productsByIds", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(ids),
+    })
+      .then((res) => res.json())
+      .then((cartProducts) => {
+        let savedCart = [];
+        for (const id in storedCart) {
+          const addedProduct = cartProducts.find(
+            (product) => product._id === id
+          );
+          if (addedProduct) {
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
+          }
+        }
+        setCart(savedCart);
+      });
+   
+  }, []);
 
   const handleAddToCart = (product) => {
     // const newCart = [...cart, product]
@@ -89,18 +113,24 @@ const Shop = () => {
       </div>
       {/* pagination button */}
       <div className="pagination">
-        <p>currentPage: {currentPage} And Items Per Pages: {itemsperPages}</p>
+        <p>
+          currentPage: {currentPage} And Items Per Pages: {itemsPerPages}
+        </p>
         {pagesNumber.map((number) => (
           <button
             onClick={() => setCurrentPage(number)}
-            className={currentPage===number ?'selected':''}
+            className={currentPage === number ? "selected" : ""}
             key={number}
           >
             {number}
           </button>
         ))}
-        <select value={itemsperPages} onChange={handleSelectChange}>
-          {options.map(option => <option key={option} value={option}>{option}</option>)}
+        <select value={itemsPerPages} onChange={handleSelectChange}>
+          {options.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
         </select>
       </div>
     </>
--- src/components/productCartLoader/CartLoader.js
+++ src/components/productCartLoader/CartLoader.js
@@ -1,10 +1,20 @@
 import { getShoppingCart } from "../../utilities/localStorage";
 
 const cartLoader = async () => {
-  const productLoader = await fetch("http://localhost:5000/products");
-  const product = await productLoader.json();
-  // console.log(product);
   const storedCart = getShoppingCart();
+  const ids = Object.keys(storedCart);
+console.log(ids);
+  const productLoader = await fetch('http://localhost:5000/productsByIds', {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(ids),
+  });
+
+  const product = await productLoader.json();
+  console.log(product);
+  // const storedCart = getShoppingCart();
   let savedCart = [];
   for (const id in storedCart) {
     const addedProduct = product.find((pd) => pd._id === id);
